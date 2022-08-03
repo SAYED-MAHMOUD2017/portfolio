29964f634e9c64ea363e886914632bd2f1253b2a
plan-03 <PLAN-03@IEMCO-GARMENTS.COM>
2022-08-03 10:24:58 +0200

    add logo on page title, add my image in header and about sections, modified portfolio section and add notes project

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 let navbar = $("nav");
 let aboutOffset = $("#about").offset().top;
+$("nav a").click((e)=> {
+    $(e.target).addClass("active").parent().siblings().find("a").removeClass("active");
+})
 $(window).scroll(()=>{
     if ($("html, body").scrollTop() >= aboutOffset - 50) {
         navbar.css({'backgroundColor':'rgba(0, 0, 0, 1)'}) 
